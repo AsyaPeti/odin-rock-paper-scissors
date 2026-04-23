81ed09a0e6c696a0b25d27f832f08193cafcd76c
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-04-23 23:23:03 +0300

    Create a function to get a computer choice
    The game is played by two: a human player and a computer. A function
    was created to generate a random computer choice.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+// A function that randomly returns: "rock," "paper," or "scissors"
+function getComputerChoice() {
+  const random = Math.floor(Math.random() * 3) + 1;
+  console.log(random);
+  
+  if (random === 1) {
+    return "rock";
+  } else if (random === 2) {
+    return "paper";
+  } else if (random === 3) {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
