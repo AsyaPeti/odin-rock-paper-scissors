33a99196131f939892b157b42650586ffd9eb9f0
AsyaPeti <62142349+AsyaPeti@users.noreply.github.com>
2026-04-24 16:33:44 +0300

    Create a function to play a single round
    After selecting the computer and human choices, they should be
    compared. Created a function to do this and run a snippet of code based
    on the result.

--- javascript.js
+++ javascript.js
@@ -17,6 +17,56 @@ function getComputerChoice() {
 
 // This function asks the user to select "rock," "paper," or "scissors"
 function getHumanChoice() {
-  let choice = prompt("Rock, paper, or scissors?", "");
-  return choice.toLowerCase();
+  return choice = prompt("Rock, paper, or scissors?", "");
 }
+
+// This function compares players' choices and shows the results
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == "rock") {
+    if (computerChoice == "rock") {
+      console.log("It's a tie! Play again.");
+    
+    } else if (computerChoice == "paper") {
+      console.log("You lose! Rock is covered by Paper.");
+      ++computerScore;
+    
+    } else if (computerChoice == "scissors") {
+      console.log("You win! Rock crushes Scissors.");
+      ++humanScore;
+    }
+  
+  } else if (humanChoice == "paper") {
+    if (computerChoice == "rock") {
+      console.log("You win! Paper covers Rock.");
+      ++humanScore;
+    
+    } else if (computerChoice == "paper") {
+      console.log("It's a tie! Play again.");
+    
+    } else if (computerChoice == "scissors") {
+      console.log("You lose! Paper is cut by Scissors.");
+      ++computerScore;
+    }
+  
+  } else if (humanChoice == "scissors") {
+    if (computerChoice == "rock") {
+      console.log("You lose! Scissors are crushed by Rock.");
+      ++computerScore;
+    
+    } else if (computerChoice == "paper") {
+      console.log("You win! Scissors cut paper.");
+      ++humanScore;
+    
+    } else if (computerChoice == "scissors") {
+      console.log("It's a tie! Play again.");
+    }
+  }
+}
+
+const humanSelection = getHumanChoice().toLowerCase();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+console.log(humanSelection);
+console.log(computerSelection);
+console.log(humanScore);
+console.log(computerScore);
